2a05a7a5d6a5ec86f1e46d716c27656cec51d7e4
Anya497 <chi.vinny0702@gmail.com>
2023-12-25 16:58:03 +0300

    create new pagerank, delete unnecessary files

--- algorithms/pagerank.py
+++ algorithms/pagerank.py
@@ -1,8 +1,8 @@
+import scipy as sp
 import numpy as np
-from graph import Graph
 
 
-def pagerank(graph: Graph, tolerance: float = 1e-5) -> np.array:
+def pagerank(graph: sp.sparse.coo_array, tolerance: float = 1e-5, alpha: float = 0.9) -> np.array:
     '''
     Computes rank of each vertex according to its input edges
 
@@ -10,15 +10,14 @@ def pagerank(graph: Graph, tolerance: float = 1e-5) -> np.array:
     Graph object
     :param tolerance:
     Value to stop the algorithm when |current_pagerank - previous_pagerank| < tolerance
+    
     :return:
     Rank of each vertex in graph
     '''
-    last_pr = np.ones([graph.N])
-    pr = np.full([graph.N], 1 / graph.N)
-    while np.linalg.norm(pr - last_pr) >= tolerance:
+    last_pr = np.ones(graph.shape[0])
+    pr = np.ones(graph.shape[0]) / graph.shape[0]
+    outdegrees = np.squeeze(np.asarray(graph.sum(axis=1).T))
+    while np.linalg.norm(last_pr - pr) >= tolerance:
         last_pr = pr
-        pr = np.zeros([graph.N])
-        for i, v in enumerate(graph.vertices):
-            for desc, _ in v.desc:
-                pr[desc] += last_pr[i] / len(v.desc)
+        pr = alpha*((pr/outdegrees) @ graph) + (1 - alpha) / graph.shape[0]
     return pr
